7922c4c9e3c3a1e7d493730f93219e1acdb053e4
zoyron <sgrarora3@gmail.com>
2026-04-15 20:35:29 +0530

    update: basic json analysis work, and install library for parsing csv

--- cli-file-analyzer/src/index.ts
+++ cli-file-analyzer/src/index.ts
@@ -1,6 +1,7 @@
 import process from "process";
 import path from "path";
 import fs from "fs";
+import {readFile} from "fs/promises";
 import {Command} from 'commander';
 
 const program  = new Command();
@@ -12,7 +13,7 @@ program
 
 program
 .argument("<file>", "path to the file to analyze")
-.action((file:string) => {
+.action(async (file:string) => {
   // resolve the absolute path of the file
   // this turns 'data.csv' into "/Users/username/project/data.csv"
   const filePath = path.resolve(file);
@@ -31,7 +32,34 @@ program
     process.exit(1);
   }
 
-  console.log(`Analysing ${ext.toUpperCase()} file at: ${filePath}`);
+  console.log(`Processing ${ext.toUpperCase()}...`);
+
+  try {
+    /**
+     * Read the file text.
+     * Here we ask the OS to read the file, and as the OS reading the file and sending us back a promise, the sync things in the call stack will start getting executed.
+     * And after the OS has read the file, this will be sent to the event loop queue. and when the stack is emptied, only then the things from event loop are sent to the stack to get executed.
+     */
+    const rawContent = await readFile(filePath, "utf-8");
+    let data: any[] = [];
+
+    if (ext === ".json"){
+      // Parse json string into a javascript object
+      const parsedData = JSON.parse(rawContent);
+
+      // ensure that data is an array even if the JSON was just one object.
+      data = Array.isArray(parsedData) ? parsedData : [parsedData];
+    } else{
+      console.log("CSV reading logic coming in the next step");
+    }
+
+    // basic results about the analyzed data
+    console.log("----Analysis Result----");
+    console.log(`Total Rows Found: ${data.length}`);
+  } catch (error: any) {
+   console.error(`Error reading or parsing file: ${error.message}`);
+   process.exit(1);
+  }
 });
 
 // THis tells the program to actually look at what you typed in the terminal.
