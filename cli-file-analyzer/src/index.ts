53db673bf1e86cd4d9d696e5f5a32356c9c00fd3
zoyron <sgrarora3@gmail.com>
2026-04-15 17:56:37 +0530

    feat: basic file handling features like ext checkign and checking if file exists or not

--- cli-file-analyzer/src/index.ts
+++ cli-file-analyzer/src/index.ts
@@ -1,14 +1,37 @@
 import process from "process";
+import path from "path";
+import fs from "fs";
 import {Command} from 'commander';
 
 const program  = new Command();
 
-program.name('file-analyzer').description("A robust CLI tool to analyze CSV nad JSON files").version("1.0.0");
+program
+.name('file-analyzer')
+.description("A robust CLI tool to analyze CSV nad JSON files")
+.version("1.0.0");
 
-program.argument("<file>", "path to the file to analyze").action((file:string) => {
-  // this is the code that runs when teh user provides a file path
-  console.log(`Successfully captured file path: ${file}`);
-  console.log("Next, we'll check if this file actually exists");
+program
+.argument("<file>", "path to the file to analyze")
+.action((file:string) => {
+  // resolve the absolute path of the file
+  // this turns 'data.csv' into "/Users/username/project/data.csv"
+  const filePath = path.resolve(file);
+
+  // Check does this file actually exist
+  if (!fs.existsSync(filePath)){
+    console.error(`Error: The file "${file}" does not exist.`);
+    process.exit(1);
+  }
+
+  // check: is it a support file type or not
+  const ext = path.extname(filePath).toLowerCase();
+
+  if(ext !== '.csv' && ext !== ".json"){
+    console.error(`Error: Unsupported file type "${ext}". Only .csv and .json are suported.`);
+    process.exit(1);
+  }
+
+  console.log(`Analysing ${ext.toUpperCase()} file at: ${filePath}`);
 });
 
 // THis tells the program to actually look at what you typed in the terminal.
