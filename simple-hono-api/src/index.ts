71d1b2d67c6b92170c4f978ae3a6d4bcb7e89676
zoyron <sgrarora3@gmail.com>
2026-04-17 17:48:31 +0530

    feat: zod validation, 2 get request with users, dynamic routing with the id

--- simple-hono-api/src/index.ts
+++ simple-hono-api/src/index.ts
@@ -1,14 +1,30 @@
+import { z } from "zod";
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
+
+// Define the schema - the blueprint for what a User object must look like
+const UserSchema = z.object({
+  id: z.number(), // id must be a number
+  name: z.string(), // name must be a string
+  email: z.string().email(), //email must be a string AND must look like a valid email
+});
+
+/**
+ * Ask Zod to infer the typescript type from the schema
+ * Now "User" is a real typepscript type - no need to write it(the type) manually
+ */
+type User = z.infer<typeof UserSchema>;
 
 // create a Hono instance
 const app = new Hono();
 
 //Mock data - a hardcoded list of users
-const users = [
+// Annotating the user array with the User type
+const users: User[] = [
   { id: 1, name: "Alice", email: "alice@example.com" },
   { id: 2, name: "Bob", email: "bob@example.com" },
   { id: 3, name: "Charlie", email: "charlie@example.com" },
+  { id: 4, name: "Sagar", email: "Sagar@example.com" },
 ];
 
 // Defining a GET route at the root path "/"
@@ -20,6 +36,21 @@ app.get("/", (c) => {
 // GET /users - a get request at the /users route will return a complete list of the users as json.
 app.get("/users", (c) => {
   return c.json(users);
+});
+
+// Making a new route of GET /user/:id - this returns a single user matching the given id
+app.get("/users/:id", (c) => {
+  const id = Number(c.req.param("id"));
+  const user = users.find((u) => u.id === id); // here in the users array, we are finding an element where the id matches the we passed through the client
+
+  // the find function either returns the id number or returns undefined
+  // if the user was not found, we send the error mesasge along with 404
+  if (!user) {
+    return c.json({ message: "User not found" }, 404);
+  }
+
+  // if the user was found, we send the user object as JSON along with 200 code
+  return c.json(user);
 });
 
 // Start the server on port 3000
