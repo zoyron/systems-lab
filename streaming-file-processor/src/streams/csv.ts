48d254d1f15df2e3a28daa761fda615a37805705
zoyron <sgrarora3@gmail.com>
2026-05-06 10:49:26 +0530

    basic project setup and basic pipelkine code too

---
+++ streaming-file-processor/src/streams/csv.ts
@@ -0,0 +1,29 @@
+import { createReadStream, createWriteStream } from "fs";
+import { parse } from "csv-parse";
+import { stringify } from "csv-stringify";
+
+// this is an empty object to be filled as the chunks are created(this is an example comment)
+// this step converts the raw raw csv text to array/objects
+
+// create the full pipeline
+export function pipeCsvThrough(inputPath: string, outputPath: string): void {
+  const csvParser = parse({ columns: true });
+
+  // create the stringifier transform stream
+  // this step converts arrays/objects to csv text
+  const csvStringifier = stringify({ header: true }); // include the header row in output
+  const readStream = createReadStream(inputPath, { encoding: "utf8" });
+  const writeStream = createWriteStream(outputPath, { encoding: "utf8" });
+
+  // pipe() connects streams: read -> parse -> stringify -> writre
+  // it also handles backpressure automatically
+  readStream.pipe(csvParser).pipe(csvStringifier).pipe(writeStream);
+
+  // write errors at each step
+  readStream.on("error", (err) => console.error("Read error:", err.message));
+  csvParser.on("error", (err) => console.error("Read error:", err.message));
+  csvStringifier.on("error", (err) =>
+    console.error("Read error:", err.message),
+  );
+  writeStream.on("error", (err) => console.error("Read error:", err.message));
+}
